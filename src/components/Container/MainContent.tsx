4a41cab95627469e0aa0e0c5d593df379ed45e8f
AmirSeraj <amir.seraj6990@gmail.com>
2024-07-15 04:16:46 +0330

    i stock in

--- src/components/Container/MainContent.tsx
+++ src/components/Container/MainContent.tsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import { Button } from "@nextui-org/button";
+import { LegacyRef, useRef } from "react";
 
 const MainContent = () => {
-  return (
-    <div className='flex justify-center items-center w-full h-[calc(100vh-50px)]'>
-        
-    </div>
-  )
-}
+  const ImgRef = useRef<LegacyRef<HTMLDivElement>>();
+  const handleSelectImg = () => {
 
-export default MainContent
+  }
+  return (
+    <div className="flex justify-center items-center w-full h-[calc(100vh-50px)] relative">
+      <div className="absolute right-4 top-3 flex gap-2">
+        <Button color="primary" onClick={handleSelectImg}>
+          <input ref={ImgRef} type="file" className="hidden"  />
+          انتخاب طرح</Button>
+        <div className="flex flex-col gap-1">
+          <div className="w-[45px] h-[45px] border border-slate-400 rounded-lg text-xs flex justify-center items-center">
+            طرح 1
+          </div>
+          <div className="w-[45px] h-[45px] border border-slate-400 rounded-lg text-xs flex justify-center items-center">
+            طرح 2
+          </div>
+          <div className="w-[45px] h-[45px] border border-slate-400 rounded-lg text-xs flex justify-center items-center">
+            طرح 3
+          </div>
+          <div className="w-[45px] h-[45px] border border-slate-400 rounded-lg text-xs flex justify-center items-center">
+            طرح 4
+          </div>
+          <div className="w-[45px] h-[45px] border border-slate-400 rounded-lg text-xs flex justify-center items-center">
+            طرح 5
+          </div>
+        </div>
+      </div>
+      <div className="w-[360px] h-full bg-white md:p-5 p-3"></div>
+    </div>
+  );
+};
+
+export default MainContent;
